d7ca2d62163b5802f3cb6ae8082858c06db4cf4b
emmy <eabiringira@bk.rw>
2020-03-03 12:48:31 +0200

    Finishing the login page

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -8,18 +8,52 @@ function homePage() {
   return (
 
     <React.Fragment>
-      <div className="App">
+      {/* <div className="App">
         <div className="App-card">
         <img src={appLogo}  alt="logo" />
 
         </div>
+        
+      </div> */}
+
+      <div className="App-login" >
+       
+     
+<div class="container" id="container">
+
+	<div class="form-container sign-in-container">
+		<form action="#">
+			<h1>Sign in</h1>
+			<div class="social-container">
+				<a href="/" class="social"><i class="fab fa-facebook-f"></i></a>
+				<a href="/" class="social"><i class="fab fa-google-plus-g"></i></a>
+				<a href="/" class="social"><i class="fab fa-linkedin-in"></i></a>
+			</div>
+			<span>or use your account</span>
+			<input type="email" placeholder="Email" />
+			<input type="password" placeholder="Password" />
+			<a href="hhtp/social">Forgot your password?</a>
+			<button>Sign In</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			
+			<div class="overlay-panel overlay-right">
+				<h1>Hello, Developers!</h1>
+				<p>Welcome to sms gateway platform  </p>
+			
+			</div>
+		</div>
+	</div>
+</div>
         
       </div>
     <div className="App">
       <footer className="App-header">
         <img src={logo}  alt="logo" />
         <p>
-         <code>Sms Gateway</code> 
+         <code>Sms Service</code> 
         </p>
      
       </footer>
